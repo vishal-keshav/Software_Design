f9fb3b0e385f82d9e3fa4132990253684a5c1e52
Vishal Keshav <vishal.keshav.1993@gmail.com>
2019-05-22 20:13:29 +0530

    added linux c++ project for testing

---
+++ inference_on_edge/linux_test/main/main.cpp
@@ -0,0 +1,13 @@
+// Test program for linux based system
+// Testing C++ capability of tensorflow through c_api.h
+
+#include <iostream>
+#include <vector>
+#include "libs/c_api.h"
+
+using namespace std;
+
+int main(){
+
+    return 0;
+}
